11b4bfbe75fdf42f473e671e9d4fa64d632aa876
DargyLobo <dargijuli@gmail.com>
2022-05-03 22:52:35 -0500

    s

--- systempolls/settings.py
+++ systempolls/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -137,3 +138,6 @@ STATICFILE_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
